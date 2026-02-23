b752ab6ea99f37eaf69fb57e45df36cf8b352b40
Adam Muhammad Rizqy <adammr126@gmail.com>
2026-02-23 10:25:41 +0700

    latihan pertemuan 3

---
+++ form-produk.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h1>form produk</h1>
+        nama produk: <pre><input type="text" name="nama"></pre>
+        harga: <pre><input type="number" name="harga"></pre>
+        <input type="submit" value="simpan">
+    </form>
+    <?php
+        $nama = "";
+        $harga = "";
+
+        if(empty($_POST['nama']) && empty($_POST['harga'])){
+            echo "input nama dan harga produk";
+        }else{
+            echo "nama produk: ".$nama."<br>";
+            echo "harga produk: ".$harga;
+        }
+        
+    ?>
+</body>
+</html>
