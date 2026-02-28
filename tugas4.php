d7487916d832121f702172bec3e557c9ead33021
madA <adammr126@gmail.com>
2026-02-28 16:58:06 +0700

    penyalesaian tugas 4

--- tugas4.php
+++ tugas4.php
@@ -1,36 +1,36 @@
-<?php
-    class nilai{
-        public $nama;
-        public $kelas;
-        public $matkul;
-        public $nilai;
-        public $status;
-        public function status($nilai){
-            if($nilai <= 60){
-                return "lulus";
-            }else{
-                return "tidak lulus";
+    <?php
+        class nilai{
+            public $nama;
+            public $kelas;
+            public $matkul;
+            public $nilai;
+            public $status;
+            public function status($nilai){
+                if($nilai <= 60){
+                    return "lulus";
+                }else{
+                    return "tidak lulus";
+                }
             }
         }
-    }
-    $data = [
-        'namaMaha'=>["aditya","shinta","ineu","agus"],
-        'kelas'=>"si 2",
-        'matkul'=>"PBO",
-        'nilai'=>["80","73","55","80"]
-    ];
+        $data = [
+            'namaMaha'=>["aditya","shinta","ineu","agus"],
+            'kelas'=>"si 2",
+            'matkul'=>"PBO",
+            'nilai'=>["80","73","55","80"]
+        ];
 
-    $nil = new nilai();
-    $nil->nama = $data['namaMaha'];
-    $nil->kelas = $data['kelas'];
-    $nil->matkul = $data['matkul'];
-    $nil->nilai = $data["nilai"];
+        $nil = new nilai();
+        $nil->nama = $data['namaMaha'];
+        $nil->kelas = $data['kelas'];
+        $nil->matkul = $data['matkul'];
+        $nil->nilai = $data["nilai"];
 
-    for($x = 0;$x < count($data['namaMaha']); $x++){
-        echo "Nama: ".$nil->nama[$x]."<br>";
-        echo "Kelas: ".$nil->kelas."<br>";
-        echo "Kelas: ".$nil->matkul."<br>";
-        echo "Nilai: ".$nil->nilai."<br>";
-        echo "Status: <br   ><br>";
-    }
-?>
+        for($x = 0;$x < count($data['namaMaha']); $x++){
+            echo "Nama: ".$nil->nama[$x]."<br>";
+            echo "Kelas: ".$nil->kelas."<br>";
+            echo "Kelas: ".$nil->matkul."<br>";
+            echo "Nilai: ".$nil->nilai[$x]."<br>";
+            echo "Status: ".$nil->status($nil->nilai[$x])."<br><br>";
+        }
+    ?>
